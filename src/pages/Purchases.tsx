f8ab22569e8265c46b3cbb3380e44ece5059efc1
Derian Arias <derianaugustoarias@gmail.com>
2023-08-03 15:52:24 -0500

    Productos disponines (purchases)

--- src/pages/Purchases.tsx
+++ src/pages/Purchases.tsx
@@ -1,8 +1,61 @@
+import { useState } from "react"
+import "./styles/purchases.css"
+
+const Purchases: React.FC = () => {
+  // Estado para almacenar los productos agregados al carrito
+  const [carrito, setCarrito] = useState<number[]> ([]);
+  const productosDisponibles = [
+    { id: 1, nombre: 'Producto 1', descripcion: 'Descripcion del producto', precio: 29.99 },
+    //Agregar + productos aqui...
+  ];
 
 
-const Purchases = () => {
+// Función para agregar un producto al carrito
+const agregarAlCarrito = (idProducto: number ) => {
+    setCarrito([...carrito, idProducto ]);
+
+};
+
+// Calcular el total de la compra
+const calcularTotal = () => {
+    return carrito.reduce((total, idProducto) => {
+      const producto = productosDisponibles.find((p) => p.id === idProducto)
+      return total + (producto ? producto.precio : 0 );
+  }, 0);
+};
+
   return (
-    <div>Purchases</div>
+    <div>
+    <h1>Productos Disponibles</h1>
+    <div className="productos">
+      {productosDisponibles.map((producto) => (
+        <div key={producto.id} className="producto">
+          <h2>{producto.nombre}</h2>
+          <p>{producto.descripcion}</p>
+          <p>Precio: ${producto.precio}</p>
+          <button onClick={() => agregarAlCarrito(producto.id)}>Agregar al Carrito</button>
+        </div>
+      ))}
+    </div>
+
+
+    <h2>Carrito de Compras</h2>
+    <div className="carrito">
+      <ul id="lista-carrito">
+        { carrito.map((idProducto) => {
+          const producto = productosDisponibles.find((p) => p.id === idProducto);
+          return (
+            producto && (
+              <li key={producto.id}>
+                {producto.nombre} - ${producto.precio}
+              </li>
+            )
+          )
+        })}
+      </ul>
+      <p>Total: <span id="total">{ calcularTotal().toFixed(2)}</span></p>
+    </div>
+    </div>
   )
 }
 
